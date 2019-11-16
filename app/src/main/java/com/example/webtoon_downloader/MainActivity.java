e3b0663ab96793220d1aad3d296a132b6b9ae5a6
Your Name <you@example.com>
2019-11-16 21:10:51 +0900

    get thumbnail src, title

--- app/src/main/java/com/example/webtoon_downloader/MainActivity.java
+++ app/src/main/java/com/example/webtoon_downloader/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.webtoon_downloader;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
-
+    String mainURL="https://comic.naver.com/webtoon/weekday.nhn";
+    linkControl linkControl=new linkControl();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread() {
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect(mainURL).get();
+                    Elements elements=doc.select("div.col_inner");
+                    linkControl.sethtml(elements.toString());
+                }
+
+                catch (Exception ex) {
+                    Log.d("mydebug",ex.toString());
+                }
+            }
+        }.start();
+
     }
 }
