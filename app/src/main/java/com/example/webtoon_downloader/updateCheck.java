a9ef2e12c3d2b502e77a95b39fdc1032486d3a00
shojoinfo <hello111353@gmail.com>
2020-02-11 23:56:17 +0900

    background task complete

--- app/src/main/java/com/example/webtoon_downloader/updateCheck.java
+++ app/src/main/java/com/example/webtoon_downloader/updateCheck.java
@@ -5,6 +5,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.util.Log;
 
@@ -15,12 +17,21 @@ import org.jsoup.select.Elements;
 import java.util.List;
 
 import static android.content.Context.ALARM_SERVICE;
+import static android.content.Context.CONNECTIVITY_SERVICE;
 
 public class updateCheck extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("hello", "alarm check");
+        ConnectivityManager manager=(ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo network=manager.getActiveNetworkInfo();
+        if(network==null|| !network.isConnected() ||network.getType()!=ConnectivityManager.TYPE_WIFI)
+        {
+            Log.d("hello","no network");
+            makeAlarm(context);
+            return;
+        }
         new Thread(() -> {
             Room_Database db = Room_Database.getInstance(context);
             List<Room_Data> data = db.Room_DAO().selectBookmark();
@@ -28,7 +39,7 @@ public class updateCheck extends BroadcastReceiver {
                 checkEpisode(a.title, a.EpisodeLink, context);
             }
         }).start();
-        //makeAlarm(context);
+        makeAlarm(context);
     }
 
     public void checkEpisode(String webtoon, String link, Context context) {
@@ -45,11 +56,14 @@ public class updateCheck extends BroadcastReceiver {
                     Room_Database db = Room_Database.getInstance(context);
                     Room_Data data = db.Room_DAO().selectTitle(webtoon);
                     if (!data.update.equals(title)) {
+                        Log.d("hello","download check");
                         new Download(href, title, webtoon, context);
                         data.update = title;
                         db.Room_DAO().update(data);
                     }
-                    Log.d("mdg", data.title);
+                    else{
+                        Log.d("hello",data.update);
+                    }
                 }
             }
         } catch (Exception e) {
@@ -63,10 +77,9 @@ public class updateCheck extends BroadcastReceiver {
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
         //API 23 이상
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000 * 10, sender);
-            //am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000*60*5, sender);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000 *10, sender);
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             am.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, sender);
     }
 }
