5bb08734a05fb46392f80813925f35b584a7d8e3
Your Name <you@example.com>
2020-02-23 17:37:47 +0900

    added new drawer

---
+++ app/src/main/java/com/myapp/bookmark/Bookmark.kt
@@ -0,0 +1,12 @@
+package com.myapp.bookmark
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.myapp.webtoon_downloader.R
+
+class Bookmark :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bookmark_activity_main)
+    }
+}
